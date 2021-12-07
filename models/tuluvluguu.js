4f1a3ca603ea7b31974da6d7f8fd3eff717d2c1a
baterdenets <bbbatuk99@gmail.com>
2021-12-07 18:54:31 +0800

    saijruulalt

--- models/tuluvluguu.js
+++ models/tuluvluguu.js
@@ -7,8 +7,14 @@ const tuluvluguuSchema = new Schema(
     system: String,
     ner: String,
     tailbar: String,
+    delgerenguiTailbar: String,
+    /**Хийгдэх ажил нь нийт хэдэн нэгжийн ажил вэ гэдэгийг тодорхойлно */
     unelgee: Number,
+    /**Хийгдсэн ажилийн нэгж дүн */
     guitsetgeliinDun: Number,
+    /**Хийгдэж байгаа ажилийн нэгж дүн */
+    yavtsiinDun: Number,
+    /**Төлөвлөгөөт ажилийн дуусах огноо*/
     duusakhOgnoo: Date,
   },
   { timestamps: true }
